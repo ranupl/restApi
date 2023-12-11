e3f9e6b97845e97741607647d488356b65504d53
ranupl <ranupl542011@gmail.com>
2023-12-11 16:13:25 +0530

    route updated

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userController = require("../controlller/userController");
 
 router.post("/create", userController.createUser);
-router.get("/allUser", userController.getAllUser);
-router.get("/getUser/:id", userController.getById);
+router.get("/users", userController.getAllUser);
+router.get("/user/:id", userController.getById);
 router.put("/updateUser/:id", userController.updateUser);
 router.delete("/deleteUser/:id", userController.deleteUser);
 router.get("/filter", userController.filterResult);
